97840f39e67d635a02d7540f737485dc8c958139
tuliomir <tulio.mir@gmail.com>
2022-02-22 12:41:34 -0300

    fix: mocked tests ignore paths

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ["<rootDir>/src/**/*.js"],
   testMatch: ["<rootDir>/__tests__/**/*.test.js"],
+  testPathIgnorePatterns: ["<rootDir>/__tests__/integration/"],
   coverageReporters: ["text-summary", "lcov", "clover"],
   testTimeout: 180000,
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
