86f9ddf8693f32900120a27c6f7e70a44bd7d4be
Luis Helder <luislhl@gmail.com>
2022-03-30 15:46:08 -0300

    fix: decrease ws delay in integration tests

--- __tests__/integration/configuration/test.config.js
+++ __tests__/integration/configuration/test.config.js
@@ -11,7 +11,7 @@ module.exports = {
   consoleLevel: process.env.TEST_CONSOLE_LEVEL || 'silly',
 
   // Defines how long tests should wait before consulting balances after transactions
-  wsUpdateDelay: process.env.TEST_WS_UPDATE_DELAY || 10000,
+  wsUpdateDelay: process.env.TEST_WS_UPDATE_DELAY || 1000,
 
   // Defines for how long the startMultipleWalletsForTest can run
   walletStartTimeout: process.env.TEST_WALLET_START_TIMEOUT || 180000,
