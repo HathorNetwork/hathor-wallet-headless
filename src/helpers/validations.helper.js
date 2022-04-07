aec0c8f8489b0f22735532f89cad90cbbe77ad09
tuliomir <tulio.mir@gmail.com>
2022-04-07 12:02:21 -0300

    refactor: removes unused function

--- src/helpers/validations.helper.js
+++ src/helpers/validations.helper.js
@@ -10,16 +10,6 @@ function parametersValidation(req) {
   return { success: true };
 }
 
-/**
- * Checks if a string value is a 64 character hex string
- * @param {string} str String value
- * @returns {boolean} True if it is a tx hex
- */
-function isStringHex(str) {
-  return /^[0-9a-fA-F]{64}$/.test(str);
-}
-
 module.exports = {
   parametersValidation,
-  isStringHex,
 };
