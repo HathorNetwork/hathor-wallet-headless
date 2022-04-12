0396ca33cc63e2ce5d67fa174751563a850c619f
Luis Helder <luislhl@gmail.com>
2022-04-12 15:29:14 -0300

    chore: scheleton for new test

---
+++ __tests__/config.test.js
@@ -0,0 +1,10 @@
+import { config as hathorLibConfig } from '@hathor/wallet-lib';
+
+describe('wallet lib config', () => {
+  it('correctly sets the tx-mining-service api-key', () => {
+    // TODO
+    // const currentApiKey = hathorLibConfig.getTxMiningApiKey();
+
+    // console.log('current', currentApiKey);
+  });
+});
