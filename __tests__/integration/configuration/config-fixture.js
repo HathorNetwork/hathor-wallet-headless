988283fc03c015cdebd66cdd33883afda365edd4
tuliomir <tulio.mir@gmail.com>
2022-04-14 14:08:46 -0300

    docs: improves connectionTimeout explanatory comment

--- __tests__/integration/configuration/config-fixture.js
+++ __tests__/integration/configuration/config-fixture.js
@@ -22,8 +22,12 @@ const config = {
   gapLimit: null,
 
   /*
-   * Usually the connectionTimeout option should be `null`, but to increase the stability of the
-   * integration tests we are adding a very long timeout here.
+   * When we leave this as null, we use the connectionTimeout default, which is 5000, i.e. 5s.
+   *
+   * This timeout is the maximum time that the websocket waits for a PONG message after sending
+   * a PING message before assuming the full node has gone down. With the currently low performance
+   * of tests, the full node is being fully loaded and we are increasing this timeout in order to
+   * prevent any wrong websocket reload.
    */
   connectionTimeout: 30000,
   allowPassphrase: false,
