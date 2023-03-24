fabf75479dacdac5a76030be72343a117d31c6f0
tuliomir <tulio.mir@gmail.com>
2023-03-24 10:42:32 -0300

    feat: adds listenedproposals map and helpers

--- src/services/atomic-swap.service.js
+++ src/services/atomic-swap.service.js
@@ -9,6 +9,7 @@ const {
   PartialTxInputData,
   swapService,
 } = require('@hathor/wallet-lib');
+const { walletListenedProposals } = require('./wallets.service');
 
 /**
  * Assemble a transaction from the serialized partial tx and signatures
@@ -53,7 +54,58 @@ const serviceCreate = async (partialTx, password) => {
   return { proposalId: id };
 };
 
+/**
+ * Adds a new proposal to the `listenedProposals` map of a wallet.
+ * @param {string} walletId
+ * @param {string} proposalId
+ * @param {string} password
+ * @return {Promise<void>}
+ */
+const addListenedProposal = async (walletId, proposalId, password) => {
+  // Checking if this wallet map has been initialized already
+  if (!walletListenedProposals.has(walletId)) {
+    walletListenedProposals[walletId] = new Map();
+    // Will also open the websocket channel
+  }
+
+  const listenedProposals = walletListenedProposals.get(walletId);
+  listenedProposals.set(proposalId, { proposalId, password });
+  // Will also add the proposalId to the Atomic Swap Service websocket channel
+};
+
+/**
+ * Removes a proposal from the `listenedProposals` map of a wallet
+ * @param {string} walletId
+ * @param {string} proposalId
+ * @return {Promise<void>}
+ */
+const removeListenedProposal = async (walletId, proposalId) => {
+  // If this wallet map has not been initialized, just ignore the request
+  if (!walletListenedProposals.has(walletId)) {
+    return;
+  }
+
+  const listenedProposals = walletListenedProposals.get(walletId);
+  listenedProposals.delete(proposalId);
+  // Will also remove the proposalId from the Atomic Swap Service websocket channel
+  // If this was the last proposal to be removed, will also delete the channel itself
+};
+
+/**
+ * Removes the `listenedProposals` map for a wallet, if it exists
+ * @param {string} walletId
+ * @return {Promise<void>}
+ */
+const removeAllWalletProposals = async walletId => {
+  // Remove all of the listened proposals
+  walletListenedProposals.delete(walletId);
+  // Will also close the websocket channel
+};
+
 module.exports = {
   assembleTransaction,
   serviceCreate,
+  addListenedProposal,
+  removeListenedProposal,
+  removeAllWalletProposals,
 };
