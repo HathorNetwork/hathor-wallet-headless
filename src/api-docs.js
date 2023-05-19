d9f2e43a5413a989f1a8cd7952bc0766ea7714bd
Pedro Ferreira <pedro@hathor.network>
2023-05-18 22:03:48 -0300

    chore: bump to v0.21.0 (#287)

--- src/api-docs.js
+++ src/api-docs.js
@@ -6,7 +6,7 @@ const apiDoc = {
   info: {
     title: 'Headless Hathor Wallet API',
     description: 'This wallet is fully controlled through an HTTP API.',
-    version: '0.20.0',
+    version: '0.21.0',
   },
   produces: ['application/json'],
   components: {
