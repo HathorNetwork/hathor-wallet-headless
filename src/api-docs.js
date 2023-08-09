5ed2988981fb1e07421c2bfc2ca6144643b0dcc9
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-08-09 17:36:31 +0100

    chore: update api-docs

--- src/api-docs.js
+++ src/api-docs.js
@@ -581,7 +581,6 @@ const apiDoc = {
                           {
                             decoded: {
                               address: 'Wk2j7odPbC4Y98xKYBCFyNogxaRimU6BUj',
-                              mine: true,
                               timelock: null,
                             },
                             token: '00',
@@ -590,6 +589,7 @@ const apiDoc = {
                             token_data: 0,
                             script: 'dqkUISAnpOn9Vo269QBvOfBeWJTLx82IrA==',
                             type: 'p2sh',
+                            mine: true,
                           }
                         ],
                         inputs: [
