c556f38e7904d9fe2f2badef85a5c9e95ec077ef
André Carneiro <andreluizmrcarneiro@gmail.com>
2023-08-18 13:52:25 -0300

    chore: bump version v0.22.0

--- src/api-docs.js
+++ src/api-docs.js
@@ -6,7 +6,7 @@ const apiDoc = {
   info: {
     title: 'Headless Hathor Wallet API',
     description: 'This wallet is fully controlled through an HTTP API.',
-    version: '0.22.0-rc5',
+    version: '0.22.0',
   },
   produces: ['application/json'],
   components: {
