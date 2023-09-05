7feaa49b132e3b4a45a50862ac0b0c0582c490f5
André Carneiro <andreluizmrcarneiro@gmail.com>
2023-09-05 11:00:10 -0300

    fix: linter errors

--- src/patch.js
+++ src/patch.js
@@ -7,24 +7,25 @@ export function patchExpressRouter(router) {
       return originalHandler;
     }
 
-    return function(req, res, next) {
+    function wrappedHandler(req, res, next) {
       const ret = originalHandler.call(this, req, res, next);
       if (ret.catch) {
-        ret.catch((err) => next(err));
+        ret.catch(err => next(err));
       }
       return ret;
-    };
+    }
+    return wrappedHandler;
   }
   const methods = ['get', 'post', 'put', 'delete'];
   for (const method of methods) {
     const original = router[method];
     const wrapper = {
-      [method]: function (path, ...callbacks) {
+      [method](path, ...callbacks) {
         return original.call(this, path, ...callbacks.map(handlerWrapper));
-      }
-    }
+      },
+    };
+    // eslint-disable-next-line
     router[method] = wrapper[method];
   }
   return router;
 }
-
