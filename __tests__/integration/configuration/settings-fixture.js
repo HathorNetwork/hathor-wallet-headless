a63f25b9b1328c98cad4e0fdc068686f9680b5e8
André Carneiro <andreluizmrcarneiro@gmail.com>
2023-09-08 13:47:45 -0300

    tests(integration): fix default config for integration tests

--- __tests__/integration/configuration/settings-fixture.js
+++ __tests__/integration/configuration/settings-fixture.js
@@ -1,18 +1,39 @@
 const { cloneDeep } = require('lodash');
 
 const defaultConfig = {
-  http_bind_address: 'fakehost',
-  http_port: 8001,
-  network: 'testnet',
-  server: 'http://fakehost:8083/v1a/',
+  http_bind_address: 'localhost',
+  http_port: 8000,
+
+  // Hathor Full-node
+  network: 'privatenet',
+  server: 'http://localhost:8083/v1a/',
+
+  // Tx Mining Service
+  txMiningUrl: 'http://localhost:8035',
+
+  // Wallet seeds
   seeds: {
-    stub_seed:
-      'upon tennis increase embark dismiss diamond monitor face magnet jungle scout salute rural master shoulder cry juice jeans radar present close meat antenna mind',
+    default: '',
   },
+
   multisig: {},
+
+  httpLogFormat: null,
+  consoleLevel: 'silly',
   tokenUid: '',
   gapLimit: null,
-  confirmFirstAddress: null,
+
+  /*
+   * When we leave this as null, we use the connectionTimeout default, which is 5000, i.e. 5s.
+   *
+   * This timeout is the maximum time that the websocket waits for a PONG message after sending
+   * a PING message before assuming the full node has gone down. With the currently low performance
+   * of tests, the full node is being fully loaded and we are increasing this timeout in order to
+   * prevent any wrong websocket reload.
+   */
+  connectionTimeout: 30000,
+  allowPassphrase: false,
+  confirmFirstAddress: false,
 };
 
 let config = cloneDeep(defaultConfig);
