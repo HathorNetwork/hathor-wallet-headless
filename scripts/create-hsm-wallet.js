a3b1480eba740acf47bfee8c0cc419b01cbd2927
tuliomir <tulio.mir@gmail.com>
2023-11-24 16:15:20 -0300

    feat: improves validation and usability

--- scripts/create-hsm-wallet.js
+++ scripts/create-hsm-wallet.js
@@ -10,6 +10,7 @@ const config = require('../src/config');
 
 /**
  * Creates a new xPriv key on the HSM
+ * @param {string} keyName Key name to be created on the HSM
  */
 async function createXprivKey(keyName) {
   // Gets the connection data from the global config file
@@ -20,6 +21,10 @@ async function createXprivKey(keyName) {
       password: config.hsmPassword,
     },
   };
+  /**
+   * Connection object
+   * @type {Hsm}
+   */
   const hsmConnection = await hsm.connect(hsmConnectionOptions);
 
   // Creates a key
@@ -42,19 +47,52 @@ async function createXprivKey(keyName) {
   return keyInfo;
 }
 
+/**
+ * Creates a promise to delay for a given number of milisseconds
+ * @param {number} [ms=1000]
+ * @returns {Promise<void>}
+ */
 async function delay(ms) {
   return new Promise(resolve => { setTimeout(resolve, ms || 1000); });
 }
 
-const keyName = 'htr_test_script';
-createXprivKey(keyName)
-  .then(async results => {
-    console.log('New xPriv Key Information:', JSON.stringify(results));
-    await delay();
-    process.exit(0);
-  })
-  .catch(async e => {
-    console.error(e.stack);
-    await delay();
-    process.exit(1);
-  });
+/**
+ * Key names on HSM must be under 32 characters and can only contain alphanumeric characters
+ * and underscores. To avoid naming issues, this method also requires the key name to be
+ * at least 3 characters long.
+ *
+ * @param {string} keyName
+ * @throws {Error} If the key name is invalid
+ * @see https://docs.hsm.dinamonetworks.io/arquitetura/chaves-objetos
+ */
+function validateKeyName(keyName) {
+  if (!keyName || keyName.length < 3) {
+    throw new Error('Invalid key name. Usage: node create-hsm-wallet.js <key-name>');
+  }
+  if (keyName.length > 32) {
+    throw new Error('Key name is too long. It must be up to 32 characters.');
+  }
+  if (!keyName.match(/^[a-zA-Z0-9_]+$/)) {
+    throw new Error('Key name can only contain alphanumeric characters and underscores.');
+  }
+}
+
+async function main() {
+  // Get the key name from the command line arguments
+  const keyName = process.argv[2];
+  validateKeyName(keyName);
+
+  return createXprivKey(keyName)
+    .then(async results => {
+      console.log('New xPriv Key Information:', JSON.stringify(results, null, 2));
+      await delay();
+      process.exit(0);
+    })
+    .catch(async e => {
+      console.error(e.stack);
+      await delay();
+      process.exit(1);
+    });
+}
+
+main();
