db7cea19042804b7efcfab6519342293e3aff07d
tuliomir <tulio.mir@gmail.com>
2024-01-23 21:02:16 -0300

    refactor: extracts start wallet procedures

--- src/services/wallets.service.js
+++ src/services/wallets.service.js
@@ -5,7 +5,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+const { Connection, HathorWallet } = require('@hathor/wallet-lib');
 const { removeAllWalletProposals } = require('./atomic-swap.service');
+const { notificationBus } = require('./notification.service');
+const { sanitizeLogInput } = require('../logger');
 
 /**
  * @type {Map<string, HathorWallet>}
@@ -44,8 +47,49 @@ async function stopAllWallets() {
   }
 }
 
+async function startWallet(walletId, walletConfig, config, options = {}) {
+  if (walletConfig.connection) {
+    throw new Error('Invalid parameter for startWallet helper');
+  }
+  const hydratedWalletConfig = { ...walletConfig };
+
+  // Builds the connection object
+  hydratedWalletConfig.connection = new Connection({
+    network: config.network,
+    servers: [config.server],
+    connectionTimeout: config.connectionTimeout,
+  });
+
+  // tokenUid is optional but if not passed as parameter the wallet will use HTR
+  if (config.tokenUid) {
+    hydratedWalletConfig.tokenUid = config.tokenUid;
+  }
+
+  const wallet = new HathorWallet(hydratedWalletConfig);
+
+  if (config.gapLimit) {
+    // XXX: The gap limit is now a per-wallet configuration
+    // To keep the same behavior as before, we set the gap limit
+    // when creating the wallet, but we should move this to the
+    // wallet configuration in the future
+    await wallet.setGapLimit(config.gapLimit);
+  }
+
+  // subscribe to wallet events with notificationBus
+  notificationBus.subscribeHathorWallet(walletId, wallet);
+
+  const info = await wallet.start();
+  // The replace avoids Log Injection
+  console.log(`Wallet started with wallet id ${sanitizeLogInput(walletId)}. \
+Full-node info: ${JSON.stringify(info, null, 2)}`);
+
+  initializedWallets.set(walletId, wallet);
+  return info;
+}
+
 module.exports = {
   initializedWallets,
   stopWallet,
   stopAllWallets,
+  startWallet,
 };
