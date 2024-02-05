ca347b03f930dadbd9abab6a896a5e84c48df27b
tuliomir <tulio.mir@gmail.com>
2024-02-05 16:53:25 -0300

    fix: test fixtures with the new constants

--- __tests__/__fixtures__/feature-fixture.js
+++ __tests__/__fixtures__/feature-fixture.js
@@ -3,6 +3,9 @@ const constants = {
 
   SWAP_SERVICE_MAINNET_BASE_URL: 'https://atomic-swap-service.mainnet.mock/',
   SWAP_SERVICE_TESTNET_BASE_URL: 'https://atomic-swap-service.testnet.mock/',
+
+  DEFAULT_PASSWORD: '123',
+  DEFAULT_PIN: '123',
 };
 
 // Allow change config at runtime
