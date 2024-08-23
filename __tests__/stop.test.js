287574f442c58fde5347aeba7ee37abc8815fc5b
André Carneiro <andreluizmrcarneiro@gmail.com>
2024-08-23 11:26:57 -0400

    tests: do not stop on some wallet states

--- __tests__/stop.test.js
+++ __tests__/stop.test.js
@@ -5,8 +5,20 @@ import { initializedWallets } from '../src/services/wallets.service';
 const walletId = 'stub_stop';
 
 describe('addresses api', () => {
-  beforeAll(async () => {
+  beforeEach(async () => {
     await TestUtils.startWallet({ walletId });
+  });
+
+  afterEach(async () => {
+    /** @type {HathorWallet} */
+    const wallet = initializedWallets.get(walletId);
+    if (!wallet) {
+      return;
+    }
+    wallet.setState(HathorWallet.ERROR);
+    await TestUtils.request
+      .post('/wallet/stop')
+      .set({ 'x-wallet-id': walletId });
   });
 
   it('should stop a wallet that has entered the ERROR state', async () => {
@@ -18,5 +30,42 @@ describe('addresses api', () => {
       .post('/wallet/stop')
       .set({ 'x-wallet-id': walletId });
     expect(response.status).toBe(200);
+    expect(response.body.success).toBe(true);
+  });
+
+  it('should not stop a wallet that is SYNCING', async () => {
+    /** @type {HathorWallet} */
+    const wallet = initializedWallets.get(walletId);
+    wallet.setState(HathorWallet.SYNCING);
+
+    const response = await TestUtils.request
+      .post('/wallet/stop')
+      .set({ 'x-wallet-id': walletId });
+    expect(response.status).toBe(200);
+    expect(response.body.success).toBe(false);
+  });
+
+  it('should not stop a wallet that is CONNECTING', async () => {
+    /** @type {HathorWallet} */
+    const wallet = initializedWallets.get(walletId);
+    wallet.setState(HathorWallet.CONNECTING);
+
+    const response = await TestUtils.request
+      .post('/wallet/stop')
+      .set({ 'x-wallet-id': walletId });
+    expect(response.status).toBe(200);
+    expect(response.body.success).toBe(false);
+  });
+
+  it('should not stop a wallet that is CLOSED', async () => {
+    /** @type {HathorWallet} */
+    const wallet = initializedWallets.get(walletId);
+    wallet.setState(HathorWallet.CLOSED);
+
+    const response = await TestUtils.request
+      .post('/wallet/stop')
+      .set({ 'x-wallet-id': walletId });
+    expect(response.status).toBe(200);
+    expect(response.body.success).toBe(false);
   });
 });
