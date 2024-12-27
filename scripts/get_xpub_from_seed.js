1d1c899f8287a74dd6b74739a60695286abc9c40
André Carneiro <andreluizmrcarneiro@gmail.com>
2024-12-27 14:44:49 -0300

    fix: exit after scripts (#517)
    * fix: exit after scripts

--- scripts/get_xpub_from_seed.js
+++ scripts/get_xpub_from_seed.js
@@ -7,18 +7,28 @@
 
 import hathorLib from '@hathor/wallet-lib';
 
-/**
- * argv contains the cli arguments that made this script run, including the interpreter
- * and script filename so to get the actual seed passed via cli we need to remove the
- * other arguments.
- * argv = ["babel-node", "get_xpub_from_seed.js", "word0", ..., "wordLast"];
- * We need to remove the first 2 and join the other arguments into a single string
- * separated by spaces
- */
-const seed = process.argv.slice(2).join(' ');
-const hdprivkeyRoot = hathorLib.walletUtils.getXPrivKeyFromSeed(seed);
-// `accountDerivationIndex` will make the util method derive to the change path
-const hdprivkey = hathorLib.walletUtils.deriveXpriv(hdprivkeyRoot, '0\'/0');
+function main() {
+  /**
+  * argv contains the cli arguments that made this script run, including the interpreter
+  * and script filename so to get the actual seed passed via cli we need to remove the
+  * other arguments.
+  * argv = ["babel-node", "get_xpub_from_seed.js", "word0", ..., "wordLast"];
+  * We need to remove the first 2 and join the other arguments into a single string
+  * separated by spaces
+  */
+  const seed = process.argv.slice(2).join(' ');
+  const hdprivkeyRoot = hathorLib.walletUtils.getXPrivKeyFromSeed(seed);
+  // `accountDerivationIndex` will make the util method derive to the change path
+  const hdprivkey = hathorLib.walletUtils.deriveXpriv(hdprivkeyRoot, '0\'/0');
 
-// Print the xpubkey
-console.log(hdprivkey.xpubkey);
+  // Print the xpubkey
+  console.log(hdprivkey.xpubkey);
+}
+
+try {
+  main();
+  process.exit(0);
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
