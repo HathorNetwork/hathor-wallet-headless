7efbdf348634d54b1d151e034085bb82dbb86699
Pedro Ferreira <pedro@hathor.network>
2025-03-24 15:24:56 -0300

    test: add test to check concurrent calls in ocb

--- __tests__/nano-contracts/on_chain_blueprint.test.js
+++ __tests__/nano-contracts/on_chain_blueprint.test.js
@@ -1,3 +1,4 @@
+import { SendTransaction } from '@hathor/wallet-lib';
 import TestUtils from '../test-utils';
 
 const walletId = 'stub_ocb';
@@ -40,5 +41,37 @@ describe('create api', () => {
       })
       .set({ 'x-wallet-id': walletId });
     expect(response2.status).toBe(400);
+  });
+
+  it('should receive an error when trying to do concurrent transactions (lock/unlock behavior)', async () => {
+    const spy = jest.spyOn(SendTransaction.prototype, 'updateOutputSelected').mockImplementation(async () => {
+      await new Promise(resolve => {
+        setTimeout(resolve, 1000);
+      });
+    });
+    try {
+      const promise1 = TestUtils.request
+        .post('/wallet/nano-contracts/create-on-chain-blueprint')
+        .send({
+          code: 'test',
+          address: 'WewDeXWyvHP7jJTs7tjLoQfoB72LLxJQqN',
+        })
+        .set({ 'x-wallet-id': walletId });
+      const promise2 = TestUtils.request
+        .post('/wallet/nano-contracts/create-on-chain-blueprint')
+        .send({
+          code: 'test2',
+          address: 'WewDeXWyvHP7jJTs7tjLoQfoB72LLxJQqN',
+        })
+        .set({ 'x-wallet-id': walletId });
+
+      const [response1, response2] = await Promise.all([promise1, promise2]);
+      expect(response1.status).toBe(200);
+      expect(response1.body.hash).toBeTruthy();
+      expect(response2.status).toBe(200);
+      expect(response2.body.success).toBe(false);
+    } finally {
+      spy.mockRestore();
+    }
   });
 });
