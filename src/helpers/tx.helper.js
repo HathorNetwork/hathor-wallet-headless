ef59aac1567c4441ec536b964bbe6e9bab7027dc
Pedro Ferreira <pedro@hathor.network>
2026-05-18 19:10:09 -0300

    feat: bump wallet lib to v3.1.0

--- src/helpers/tx.helper.js
+++ src/helpers/tx.helper.js
@@ -272,7 +272,11 @@ async function markUtxosSelectedAsInput(wallet, utxos, markAs, ttl) {
 async function runSendTransaction(sendTx, unlock) {
   try {
     if (!sendTx.transaction) {
+      // wallet-lib v3 split prepare and sign: prepareTx() builds the tx data but
+      // no longer signs or sets timestamp/weight/parents. signTx() does the
+      // signing plus prepareToSend with the network's weight constants.
       await sendTx.prepareTx();
+      await sendTx.signTx();
     }
     await sendTx.updateOutputSelected(true);
   } finally {
